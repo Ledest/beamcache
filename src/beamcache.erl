f4273cde09dfe43f36dbe37e596254012a2030c9
Led <ledest@gmail.com>
2017-04-20 14:40:37 +0300

    add init/3; update specs; replace compile:forms/2 to compile:noenv_forms/2

--- src/beamcache.erl
+++ src/beamcache.erl
@@ -1,42 +1,59 @@
 -module(beamcache).
 
--export([init/2,
+-export([init/2, init/3,
          forms/2,
          module/2, module/3]).
 
 -spec init(M::module(), B::binary()) -> {module, module()} | {error, badarg | code:load_error_rsn()};
           (M::module(), [{_, _}]|map()) -> {module, module()} | {error, badarg | code:load_error_rsn()} | error.
-init(M, B) when is_atom(M), is_binary(B) ->
-    code:purge(M),
-    code:load_binary(M, atom_to_list(M) ++ ".erl", B);
+init(M, B) when is_atom(M), is_binary(B) -> load(M, B);
 init(M, D) when is_atom(M), is_list(D) orelse is_map(D) ->
     case module(M, D) of
-        {ok, M, B} -> init(M, B);
+        {ok, M, B} -> load(M, B);
+        Error -> Error
+    end.
+
+-spec init(M::module(), B::binary(), O::code:options()) ->
+          {module, module()} | {error, badarg | code:load_error_rsn()};
+          (M::module(), [{_, _}]|map(), O::code:options()) ->
+          {module, module()} | {error, badarg | code:load_error_rsn()} | error.
+init(M, B, O) when is_atom(M), is_binary(B), is_list(O) ->
+    case proplists:get_bool(protected, O) of
+        true ->
+            true = code:unstick_mod(M),
+            R = load(M, B),
+            true = code:stick_mod(M),
+            R;
+        false -> load(M, B)
+    end;
+init(M, D, O) when is_atom(M), is_list(D) orelse is_map(D), is_list(O) ->
+    case module(M, D, proplists:delete(protected, O)) of
+        {ok, M, B} -> init(M, B, O);
         Error -> Error
     end.
 
 -spec module(M::module(), D::[{_, _}]|map()) -> {ok, module(), binary()} | error.
-module(M, D) when is_atom(M), is_list(D) orelse is_map(D) -> compile:forms(forms(M, D), [report_errors]).
+module(M, D) when is_atom(M), is_list(D) orelse is_map(D) -> compile:noenv_forms(forms(M, D), [report_errors]).
 
--spec module(M::module(), D::[{_, _}]|map(), list()) -> {ok, module(), binary()} | error.
+-spec module(M::module(), D::[{_, _}]|map(), O::code:options()) -> {ok, module(), binary()} | error.
 module(M, D, O) when is_atom(M), is_list(D) orelse is_map(D), is_list(O) ->
-    compile:forms(forms(M, D),
-                  case lists:member(report_errors, O) of
-                      true -> O;
-                      false -> [report_errors|O]
-                  end).
+    compile:noenv_forms(forms(M, D),
+                        case proplists:is_defined(report_errors, O) of
+                            true -> O;
+                            false -> [report_errors|O]
+                        end).
 
--spec forms(M::module(), [{_, _}]|map()) -> [erl_parse:abstract_form()].
+-spec forms(M::module(), [{_, _}]|map()) -> compile:abstract_code().
 forms(M, L) when is_atom(M), is_list(L) ->
     forms(M, L,
           try
               maps:from_list(L)
           catch
-              error:badarg -> error(badarg, [L])
+              error:badarg -> error(badarg, [M, L])
           end);
 forms(M, D) when is_atom(M), is_map(D) -> forms(M, D, D).
 
--spec forms(M::module(), D::[{_, _}]|map(), Map::map()) -> [erl_parse:abstract_form()].
+-spec forms(M::module(), D::[{_, _}]|map(), Map::map()) -> compile:abstract_code().
 forms(M, D, Map) ->
     [{attribute, 1, file, {atom_to_list(M) ++ ".erl", 1}},
      {attribute, 1, module, M},
@@ -47,8 +64,7 @@ forms(M, D, Map) ->
      {function, 8, get, 1,
       [{clause, 8, [{var, 8, 'K'}], [],
         [{'try', 9,
-          [{call, 10, {remote, 10, {atom, 10, maps}, {atom, 10, get}},
-            [{var, 10, 'K'}, erl_parse:abstract(Map, 10)]}],
+          [{call, 10, {remote, 10, {atom, 10, maps}, {atom, 10, get}}, [{var, 10, 'K'}, erl_parse:abstract(Map, 10)]}],
           [],
           [{clause, 11,
             [{tuple, 12, [{atom, 12, error}, {tuple, 12, [{atom, 12, badkey}, {var, 12, '_'}]}, {var, 12, '_'}]}],
@@ -61,3 +77,8 @@ forms(M, D, Map) ->
         [{call, 15, {remote, 15, {atom, 15, maps}, {atom, 15, get}},
           [{var, 15, 'K'}, erl_parse:abstract(Map, 15), {var, 15, 'D'}]}]}]},
      {eof, 16}].
+
+-spec load(M::module(), B::binary()) -> {module, module()} | {error, badarg | code:load_error_rsn()}.
+load(M, B) ->
+    code:purge(M),
+    code:load_binary(M, atom_to_list(M) ++ ".erl", B).
